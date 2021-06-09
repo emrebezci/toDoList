c37f4797b7dd23bf24ea916b63619ec0cf558ce5
emrebezci <mrbzcie@gmail.com>
2021-06-09 18:47:37 +0300

    create border when mouseover

--- js/main.js
+++ js/main.js
@@ -6,13 +6,18 @@ function yeniEleman(){
     document.getElementById("list").innerHTML += li;
     document.getElementById("inputumuz").value = ""; // clear the value
     document.getElementById("list").style.listStyleType = "none";
+
 }
 
-// on click delete listed item
-let sil = document.getElementById("list");
-sil.addEventListener("click", function (sil){
-    sil.target.innerHTML = "";
+
+
+// on click create a line on words
+let ciz = document.getElementById("list");
+ciz.addEventListener("click", function (ciz){
+    ciz.target.style.textDecoration = "line-through";
 });
+
+
 
 
 
@@ -24,13 +29,22 @@ let test = document.getElementById("list");
 test.addEventListener("mouseover", function (event) {
     // highlight the mouseover target
     event.target.style.color = "white";
-
-
     // reset the color after a short delay
     setTimeout(function () {
         event.target.style.color = "";
     }, 500);
 }, false);
+
+// delete border after short delay
+let border = document.getElementById("list");
+test.addEventListener("mouseover", function (addborder){
+    addborder.target.style.border = "0.5px solid";
+    setTimeout(function () {
+        addborder.target.style.border = "none";
+    }, 500);
+    
+});
+
 
 
 
