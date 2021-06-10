e1ca45fa445b0e8b8a8656c674c1eb54a7d9d611
emrebezci <mrbzcie@gmail.com>
2021-06-10 17:20:02 +0300

    added 1 function, on double click remove the lines on words

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function yeniEleman(){
     var text = document.getElementById("inputumuz").value;
-    var li = "<li>" + text + " </li>";
-    // var addspan =""+" <span id='silme' onclick='this.parentNode.style.display = 'none'; ' class='closebtn'>&times;</span>";
-    // document.getElementById("list").innerHTML += addspan; // span eklendi fakat style ekliyemedim
+    var li = "<li>" + text +"</li>";
+    var addspan = "" + "<span class='close'>X</span>";
+    document.getElementById("list").innerHTML += addspan;
     document.getElementById("list").innerHTML += li;
     document.getElementById("inputumuz").value = ""; // clear the value
     document.getElementById("list").style.listStyleType = "none";
@@ -11,11 +11,19 @@ function yeniEleman(){
 
 
 
+
 // on click create a line on words
-let ciz = document.getElementById("list");
-ciz.addEventListener("click", function (ciz){
+var ciz = document.getElementById("list");
+    ciz.addEventListener("click", function (ciz){
     ciz.target.style.textDecoration = "line-through";
 });
+//  on doubleclick remove line on words
+var cizme = document.getElementById("list");
+    cizme.addEventListener("dblclick", function (cizme){
+        cizme.target.style.textDecoration = "none";
+    })
+
+
 
 
 
@@ -28,7 +36,7 @@ let test = document.getElementById("list");
 // is moved over a different list item
 test.addEventListener("mouseover", function (event) {
     // highlight the mouseover target
-    event.target.style.color = "white";
+    event.target.style.color = "orange";
     // reset the color after a short delay
     setTimeout(function () {
         event.target.style.color = "";
@@ -38,12 +46,29 @@ test.addEventListener("mouseover", function (event) {
 // delete border after short delay
 let border = document.getElementById("list");
 test.addEventListener("mouseover", function (addborder){
-    addborder.target.style.border = "0.5px solid";
+    addborder.target.style.border = "0.1px solid";
     setTimeout(function () {
         addborder.target.style.border = "none";
     }, 500);
-    
 });
+
+
+//  close button
+// yeni eklenen elemanlarda kapat butonu çalışmıyor BUG
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    };
+}
+
+
+function myFunction() {
+    var element = document.getElementById("list");
+    element.target.classList.toggle("mystyle");
+}
 
 
 
